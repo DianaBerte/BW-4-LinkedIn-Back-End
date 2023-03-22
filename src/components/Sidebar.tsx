b8130e6d2ecf8b1331ad456c60bbc34bc042ed78
ajeer333 <ajeer333@gmail.com>
2023-03-22 09:38:39 +0100

    commit

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -48,7 +48,7 @@ const Sidebar = () => {
   const generateRandomNumbers = () => {
     const newNumbers: number[] = [];
     while (newNumbers.length < 20) {
-      const randomNumber = Math.floor(Math.random() * 25);
+      const randomNumber = Math.floor(Math.random() * 25 + 1);
       if (!newNumbers.includes(randomNumber)) {
         newNumbers.push(randomNumber);
       }
